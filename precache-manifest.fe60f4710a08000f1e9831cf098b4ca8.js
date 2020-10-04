7637e325f3ca71f422bb0e1c4f51e38e376ba4ef
makkiboukhris <makki01boukhris@gmail.com>
2020-10-04 20:16:01 +0100

    Updates

---
+++ precache-manifest.fe60f4710a08000f1e9831cf098b4ca8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e94b458bfafb724706ecc849366ac7",
+    "url": "/router/index.html"
+  },
+  {
+    "revision": "4eb2bc5b8f1ee8b44130",
+    "url": "/router/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3515abed8eefc8b44c9f",
+    "url": "/router/static/css/main.6452e301.chunk.css"
+  },
+  {
+    "revision": "4eb2bc5b8f1ee8b44130",
+    "url": "/router/static/js/2.793b9815.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/router/static/js/2.793b9815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3515abed8eefc8b44c9f",
+    "url": "/router/static/js/main.7d09403e.chunk.js"
+  },
+  {
+    "revision": "98694bf83ec4d1d1cf96",
+    "url": "/router/static/js/runtime-main.1431bb78.js"
+  }
+]);
